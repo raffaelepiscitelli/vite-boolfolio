070449e2bc169b867a09a8ec52987c01e099c951
raffaelepiscitelli <raffaelepiscitelli1994@gmail.com>
2024-02-21 18:30:34 +0100

    Creazione progetto

---
+++ vite-boolfolio/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <h1>Ciao</h1>
+  </header>
+
+  <main>
+
+  </main>
+</template>
+
+<style lang="scss">
+  @use './style/general.scss' as *;
+</style>
